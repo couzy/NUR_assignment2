50652e81d426d1dd3ed710af48ea7ddb48f66904
couzy <48790022+couzy@users.noreply.github.com>
2019-05-24 16:15:09 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,6 +13,12 @@ if [ ! -e randomnumbers.txt ]; then
   wget https://home.strw.leidenuniv.nl/~nobels/coursedata/randomnumbers.txt
 fi
 
+echo "Download data for quad-tree particles"
+if [ ! -e randomnumbers.txt ]; then
+  wget strw.leidenuniv.nl/~nobels/coursedata/colliding.hdf5
+fi
+
+echo "Download data for GRB classification"
 if [ !-e GRBs.txt]; then
   wget strw.leidenuniv.nl/~nobels/coursedata/GRBs.txt
 fi
@@ -40,6 +46,10 @@ python3 exercise5.py > exercise5.txt
 # Script that saves data to a file
 echo "Run the sixth script ..."
 python3 exercise6.py > exercise6.txt
+
+echo "Run the seventh script ..."
+python3 exercise7.py > exercise7.txt
+
 
 ffmpeg -framerate 30 -pattern_type glob -i "plots/snap*.png" -s:v 640x480 -c:v libx264 -profile:v high -level 4.0 -crf 10 -tune animation -preset slow -pix_fmt yuv420p -r 25 -threads 0 -f mp4 Zeldovich2d.mp4
 
